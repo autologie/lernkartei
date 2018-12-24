4a318648f8b78961fec6b26628dc5e7c8efb5a3f
suguru <sgr.inoue@gmail.com>
2018-12-24 22:36:47 +0900

    register service worker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {Elm} from './Main.elm';
 import dictUrl from './dict.tsv';
+import {register} from 'register-service-worker';
 
 const app = Elm.Main.init({flags: [dictUrl, Date.now()]});
 
@@ -16,4 +17,30 @@ new MutationObserver(mutations => {
 }).observe(document.documentElement, {
   subtree: true,
   attributes: true,
+});
+
+register('/service-worker.js', {
+  ready(registration) {
+    console.log('Service worker is active.');
+  },
+  registered(registration) {
+    console.log('Service worker has been registered.');
+  },
+  cached(registration) {
+    console.log('Content has been cached for offline use.');
+  },
+  updatefound(registration) {
+    console.log('New content is downloading.');
+  },
+  updated(registration) {
+    console.log('New content is available; please refresh.');
+  },
+  offline() {
+    console.log(
+      'No internet connection found. App is running in offline mode.',
+    );
+  },
+  error(error) {
+    console.error('Error during service worker registration:', error);
+  },
 });
